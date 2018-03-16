e921389cb03ba48db8879280e32230fa590d8273
Windows <gersoncalienes@gmail.com>
2018-03-16 17:31:35 -0400

    Connected Hero Slider over to main App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import HeroContainer from './components/Hero_Container';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <HeroContainer />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
---
+++ src/components/Hero_Container.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class HeroContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Main Div</h3>
+        <h1>Big Diver</h1>
+      </div>
+    );
+  }
+}
