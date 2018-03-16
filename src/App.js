f8e36266aa26516f96313c8e6759400c0e447959
Windows <gersoncalienes@gmail.com>
2018-03-16 18:37:09 -0400

    Added Main Container component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-// import './styles/App.css';
 import './styles/hero-slider-style.css';
 import './styles/reset.css';
 
 import HeroContainer from './components/Hero_Container';
+import MainContainer from './components/Main_Container';
 
 class App extends Component {
   render() {
     return (
       <div>
         <HeroContainer />
+        <MainContainer />
       </div>
     );
   }
---
+++ src/components/Main_Container.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const MainContainer = () => (
+  <main class="cd-main-content">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit veniam
+      quisquam, rem illum dicta cumque, voluptate fugiat impedit beatae rerum
+      ratione, voluptates nisi magni delectus ab, eaque atque iste. Molestias
+      incidunt nemo veniam alias nam nisi distinctio optio error architecto
+      odit! Illo dicta nulla fugiat distinctio laudantium, corrupti eum unde.
+    </p>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit veniam
+      quisquam, rem illum dicta cumque, voluptate fugiat impedit beatae rerum
+      ratione, voluptates nisi magni delectus ab, eaque atque iste. Molestias
+      incidunt nemo veniam alias nam nisi distinctio optio error architecto
+      odit! Illo dicta nulla fugiat distinctio laudantium, corrupti eum unde.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit veniam
+      quisquam, rem illum dicta cumque, voluptate fugiat impedit beatae rerum
+      ratione, voluptates nisi magni delectus ab, eaque atque iste. Molestias
+      incidunt nemo veniam alias nam nisi distinctio optio error architecto
+      odit! Illo dicta nulla fugiat distinctio laudantium, corrupti eum unde.
+    </p>
+  </main>
+);
+
+export default MainContainer;
