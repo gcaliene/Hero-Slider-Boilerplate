5c35c368ad9734e56fc2f78f860c9c97c8e6b01b
Windows <gersoncalienes@gmail.com>
2018-03-16 17:39:54 -0400

    Added the main styles for Hero Slider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 
 import HeroContainer from './components/Hero_Container';
 
